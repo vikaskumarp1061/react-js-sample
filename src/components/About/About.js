10ad361fd2ad349aa645cb6470294dbfae29aff0
thisispradeepk <50820849+thisispradeepk@users.noreply.github.com>
2022-07-02 21:28:17 +0530

    Update About.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,7 +4,7 @@ class About extends Component {
   render() {
     return (
       <div>
-        <h1>This is About page.</h1>
+        <h1>This is About pageeeeeeeeeeeeeeeee.</h1>
         <h2>Hi~~~</h2>
       </div>
     );
