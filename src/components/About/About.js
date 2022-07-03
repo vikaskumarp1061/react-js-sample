efbc987783bebd9ced54209c7fb2500bd92546f0
thisispradeepk <50820849+thisispradeepk@users.noreply.github.com>
2022-07-03 23:22:05 +0530

    Update About.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,7 +4,7 @@ class About extends Component {
   render() {
     return (
       <div>
-        <h1>This is About page. this is new commit1</h1>
+        <h1>This is About page. this is my final commit,please watch it..</h1>
         <h2>Hi~~~</h2>
       </div>
     );
